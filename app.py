5623daaa13b55cd0d5eacc7a97001d0f00cd7c10
Hrithik Agarwal <hrithikinfinite@gmail.com>
2021-06-24 22:00:14 +0530

    removed comments

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import pickle
 import string
-# import nltk
-# nltk.download()
-# from nltk.corpus import stopwords
 from stop_words import get_stop_words
 from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-# stopwords = pickle.load(open('stp.pkl','rb'))
 lem = pickle.load(open('lem.pkl','rb'))
 ps = pickle.load(open('pm.pkl','rb'))
 vect = pickle.load(open('vect.pkl','rb'))
 stopwords = get_stop_words('en')
+model = pickle.load(open('model.pkl','rb'))
+
 
 def func3(text):
     no_punc = [char for char in text if char not in string.punctuation]
@@ -23,13 +21,6 @@ def func3(text):
     str_words = [word for word in lem_words]
     str_words = ' '.join(str_words)
     return str_words
-
-model = pickle.load(open('model.pkl','rb'))
-# # f1 = pickle.load(open('f1.pkl','rb'))
-# f2 = pickle.load(open('f2.pkl','rb'))
-# f3 = pickle.load(open('pm.pkl','rb'))
-# f4 = pickle.load(open('lem.pkl','rb'))
-# f5 = pickle.load(open('stp.pkl','rb'))
 
 
 
@@ -52,9 +43,6 @@ def subj():
     else:
         return(render_template('index.html', ans = 'SPAM'))
 
-
-
-    # return render_template('index.html', ans = 1)
 
 if __name__ == '__main__':
     app.run(debug = True)
